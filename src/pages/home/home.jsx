db59ff026c1bc74adb0083174aff7b15ae7e3c7b
gotoo123 <1219727892@qq.com>
2022-07-25 19:57:37 +0800

    fix: 修改页面滚动条问题

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,21 +4,24 @@ import './home.css'
 const Home = () => {
   return (
     <>
-      <header className="header-container">
-        <Header/>
-        <div className="content">
-          <div className="title">
-            Just4u
+      <div className="wrapper">
+        <header className="header-container">
+          <Header/>
+          <div className="content">
+            <div className="title">
+              Just4u
+            </div>
+            <div className="description">
+              No Pain No Gain
+            </div>
           </div>
-          <div className="description">
-            No Pain No Gain
-          </div>
-        </div>
-        <Wave/>
-      </header>
-      <section className="content-container">
+          <Wave/>
+        </header>
+        <section className="content-container">
 
-      </section>
+        </section>
+      </div>
+
 
     </>
   )
