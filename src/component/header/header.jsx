4bf0fd9c91b991fc09a2d1e2e36852e7d228ce10
“gotoo123” <1219727892@qq.com>
2022-08-03 17:29:24 +0800

    feat: 增加页头跳转逻辑

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,20 +1,26 @@
 import React, { useContext } from 'react';
 import './header.css';
 import LocaleContext from '../locale-provider/context';
+import {Link} from "react-router-dom";
 
 const Header = () => {
-  const value = useContext(LocaleContext);
-  const navList = Object.values(value);
+  const res = useContext(LocaleContext);
+  const list = [];
+  Object.keys(res).forEach((key) => {
+    list.push([key, res[key]]);
+  })
 
   return (
     <>
       <div className="header-content">
         <div className="header-logo">Just4u</div>
         <div className="header-nav">
-          {navList.map((item) => (
-            <span className="nav-item" key={item}>
-              {item}
-            </span>
+          {list.map((item) => (
+            <Link to={`/${item[0]}`} key={item[0]}>
+              <span className="nav-item">
+                {item[1]}
+              </span>
+            </Link>
           ))}
         </div>
       </div>
