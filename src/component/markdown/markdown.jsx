4a31eedb3e34c0febc89ce7c9f6ad04e5d0a9321
“gotoo123” <1219727892@qq.com>
2024-01-12 17:48:15 +0800

    feat: 引入md css样式

--- src/component/markdown/markdown.jsx
+++ src/component/markdown/markdown.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { marked } from 'marked';
 import PropTypes from 'prop-types';
-import styles from './markdown.less';
+// import styles from './markdown.less';
+import styles from './markdown-1.css';
 
 const Markdown = (props) => {
   const a = marked.parse(props.md);
   return (
     <>
-      <div className={styles.markdownContent} dangerouslySetInnerHTML={{ __html: a }} />
+      <div className={styles.markdownBody} dangerouslySetInnerHTML={{ __html: a }} />
     </>
   );
 };
